8fe2d40ff6a29116987e2ff83e431a292c1e09c2
Mateus Rech <mateusrech255@gmail.com>
2026-05-09 11:54:16 -0300

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Verifique se no GitHub termina com 'agencia' ou 'agency'
-  site: 'https://mateusrech255-sketch.github.io/site-da-fp-agencia',
-  base: '/site-da-fp-agencia',
+  site: 'https://mateusrech255-sketch.github.io/site-da-fp-agency',
+  base: '/site-da-fp-agency',
 });
