9518604556863b5e7f3e1dfdd87fefa9251d2ca4
Mateus Rech <mateusrech255@gmail.com>
2026-05-08 21:14:06 -0300

    Configura GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://mateusrech255-sketch.github.io/site-da-fp-agency',
+});
